cfc4e6c19fef7c0b907c6a13fb0889e74e3a45e7
SantiagoWoznyn <135929395+SantiagoWoznyn@users.noreply.github.com>
2025-01-23 13:56:44 -0300

    Update nexterm_deploy.sh

--- script rpi4/dockersc/Access/nexterm/nexterm_deploy.sh
+++ script rpi4/dockersc/Access/nexterm/nexterm_deploy.sh
@@ -7,21 +7,25 @@ PORT=${PORT:-6989}
 # Solicitar el path del volumen
 read -p "Ingresa la ruta para el volumen (por defecto ./nexterm_data): " VOLUME_PATH
 VOLUME_PATH=${VOLUME_PATH:-./nexterm_data}
-VOLUME_PATH=$(realpath "$VOLUME_PATH")  # Esto obtiene la ruta absoluta
+VOLUME_PATH=$(realpath "$VOLUME_PATH")  # Obtener la ruta absoluta
 
 # Crear el archivo docker-compose.yml
-echo "services:
+cat <<EOF > docker-compose.yml
+services:
   nexterm:
     ports:
-      - \"$PORT:$PORT\"
+      - "$PORT:$PORT"
     restart: always
     volumes:
-      - \"$VOLUME_PATH:/app/data\"
+      - "$VOLUME_PATH:/app/data"
     image: germannewsmaker/nexterm:1.0.2-OPEN-PREVIEW
 volumes:
-  nexterm:" > docker-compose.yml
+  nexterm:
+EOF
 
 echo "Archivo docker-compose.yml generado con éxito."
 
+# Ejecutar docker-compose up -d
 echo "Ejecutando docker-compose up -d..."
-docker-compose up -d
+sudo docker-compose up -d
+
